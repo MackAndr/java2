e8315ba0ddb08ef9dc19dac86f524cfb552584a7
MackAndr <111796414+MackAndr@users.noreply.github.com>
2023-02-17 03:33:07 +0400

    Add files via upload

---
+++ homework5/main.java
@@ -0,0 +1,10 @@
+package homework5;
+
+import homework5.Controller.MathController;
+
+public class main {
+    public static void main(String[] args) {
+        MathController mathController = new MathController();
+        mathController.execute();
+    }
+}
